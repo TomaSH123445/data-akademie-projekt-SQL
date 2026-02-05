8ece2ce77dcbb1d2f1da83a3c274ed13cba204ad
Tomáš Havelec <havelect@gmail.com>
2026-02-05 15:43:13 +0100

    Update otazka_1.sql

--- otazka_1.sql
+++ otazka_1.sql
@@ -1,8 +1,23 @@
 -- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
-SELECT 
-    rok,
-    ROUND(prumerna_mzda, 2) AS prumerna_mzda,
-    LAG(prumerna_mzda) OVER (ORDER BY rok) AS predchozi_mzda,
-    ROUND((prumerna_mzda / LAG(prumerna_mzda) OVER (ORDER BY rok) - 1) * 100, 2) AS mezirocni_zmena_procent
-FROM t_tomas_havelec_project_sql_primary_final
-ORDER BY rok;
+WITH mzdy AS (
+  SELECT
+    year,
+    industry_branch_code,
+    MAX(industry_branch_name) AS industry_branch_name,
+    MAX(avg_wage_czk) AS prumerna_mzda
+  FROM t_tomas_havelec_project_sql_primary_final
+  WHERE industry_branch_code <> 'ALL'
+  GROUP BY year, industry_branch_code
+)
+SELECT
+  year,
+  industry_branch_code,
+  industry_branch_name,
+  ROUND(prumerna_mzda, 2) AS prumerna_mzda,
+  ROUND(LAG(prumerna_mzda) OVER (PARTITION BY industry_branch_code ORDER BY year), 2) AS predchozi_mzda,
+  ROUND(
+    (100 * (prumerna_mzda / NULLIF(LAG(prumerna_mzda) OVER (PARTITION BY industry_branch_code ORDER BY year), 0) - 1))::numeric,
+    2
+  ) AS mezirocni_zmena_procent
+FROM mzdy
+ORDER BY industry_branch_code, year;
